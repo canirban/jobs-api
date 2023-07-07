ead74103f91117be67a681a9b2f157c6fe5c46c7
canirban <anirbanc6617@gmail.com>
2023-07-07 21:40:17 +0100

    feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,12 @@
-const register = (req, res) => {
-  res.send("Register User");
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
+
+const register = async (req, res) => {
+  const createdUser = await User.create({ ...req.body });
+  const { password, _id, __v, ...others } = createdUser._doc;
+  others.token = createdUser.createJWT();
+  res.status(StatusCodes.CREATED).send({ user: others });
 };
 const login = (req, res) => {
   res.send("Login User");
