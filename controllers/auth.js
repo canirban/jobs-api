6d322f978de249a1eaaec3be9cce5f65d2188301
canirban <anirbanc6617@gmail.com>
2023-07-07 11:50:20 +0100

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,8 @@
+const register = (req, res) => {
+  res.send("Register User");
+};
+const login = (req, res) => {
+  res.send("Login User");
+};
+
+module.exports = { register, login };
---
+++ controllers/jobs.js
@@ -0,0 +1,17 @@
+const getAllJobs = (req, res) => {
+  res.send("Get All Jobs");
+};
+const getJob = (req, res) => {
+  res.send("Get Job");
+};
+const createJob = (req, res) => {
+  res.send("Login User");
+};
+const updateJob = (req, res) => {
+  res.send("Login User");
+};
+const deleteJob = (req, res) => {
+  res.send("Login User");
+};
+
+module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
